de0c236addc3bd51815f88d8747dd5c84a5c0b38
Prathik Mishra <126965610+Prathik-mishra@users.noreply.github.com>
2024-08-21 19:48:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let arr=[];
+let largest=arr[0];
+let secondlargest=-1;
+for(let i=1;i<arr.length;i++){
+    if(arr[i]>largest){
+secondlargest=largest;
+largest=arr[i];
+    }
+    else if(largest<arr[i]&&secondlargest>arr[i]){
+        secondlargest=arr[i]
+    }
+   
+}
+console.log(secondlargest);
